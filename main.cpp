bd9ea0287d98b4651591ca048d5e6dd9d0b25a5e
DonDubstep <vasyunin.m@gmail.com>
2025-02-23 14:58:23 +0300

    Добавил задач и исправил иконку

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    w.setWindowIcon(QIcon(QCoreApplication::applicationDirPath() + "/src/app_icon.ico")); // Укажите путь к иконке
+    w.setWindowIcon(QIcon(QCoreApplication::applicationDirPath() + "/src/skill_wheel_icon.ico")); // Укажите путь к иконке
     return a.exec();
 }
