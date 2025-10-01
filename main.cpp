fd51719b552cf8a3f4a7f387b44a5ae67994575e
DonDubstep <vasyunin.m@gmail.com>
2025-10-01 14:02:59 +0300

    Сделано ограничение размера

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setMinimumSize(270,300);
     w.show();
     w.setWindowIcon(QIcon(QCoreApplication::applicationDirPath() + "/src/skill_wheel_icon.ico")); // Укажите путь к иконке
     return a.exec();
